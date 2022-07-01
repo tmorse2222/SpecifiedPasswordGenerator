2115af10c33099d142f5591e77ff9e4ee04040e8
Tyler M <tmorse2222@gmail.com>
2022-07-01 15:14:51 -0700

    alterted pop-up text

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Function generatePassword
 function generatePassword() {
-  window.prompt(`Please select password length`);
-  
+  window.prompt(`Please enter a number 8-128, this will be your password length`);
+
 }
 
 // Write password to the #password input
