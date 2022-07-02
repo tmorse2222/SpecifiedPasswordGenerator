ae886c5526c85f3b0815a7ad7446bfd0246a645d
Tyler M <tmorse2222@gmail.com>
2022-07-01 18:10:18 -0700

    added confirmation popup

--- assets/script.js
+++ assets/script.js
@@ -9,18 +9,23 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
  var passwordLength = window.prompt(`Please enter a number 8-128, this will be your password length`);
  if (7 < Number(passwordLength) && Number(passwordLength) < 128) {
-  var passwordCase = window.confirm(`You have selected a password lenght of : ${passwordLength}\n
+  var passwordCase = window.confirm(`Your password will contain : ${passwordLength} characters\n
   Please select "ok" if you would like to include both upper and lower case characters in your password. If you would like to use lower case only, please select cancel.`)
   if (passwordCase = true) {
     var passwordNumber = window.confirm(`You have selected both Upper and Lower case characters\n
-    You have selected a password length of : ${passwordLength}\n
+    Your password will contain : ${passwordLength} characters\n
     Please select "ok" if you would like to include numbers in your password, please select cancel if you would like no numbers`)
     if (passwordNumber = true) {
       var passwordSpecial = window.confirm(`You have selected to include numbers, Upper case, and Lower case characters in your password\n
-      You have selected a password length of : ${passwordLength}\n
+      Your password will contain : ${passwordLength} characters\n
       Please select "ok" if you would like to include special characters, such as !, @, #, if not please select cancel`)
       if (passwordSpecial = true) {
-        return passwordLength;
+        var passwordConfirm = window.confirm(`You have selected a password that will contain special characters, numbers, upper, and lower case characters\n
+        Your password will contain : ${passwordLength} characters\n
+        To confirm, please select "ok", to start over, please select "cancel"`)
+        if (passwordConfirm = true){
+          return passwordLength;
+        }
       }
     }
   } else {
